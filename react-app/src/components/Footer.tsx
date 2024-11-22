2a78d03b7688f30fd55ccd290f41ccddf95b943d
maanbanaan <manuel.dessing@gmail.com>
2024-11-22 17:54:24 +0100

    updated footer

--- react-app/src/components/Footer.tsx
+++ react-app/src/components/Footer.tsx
@@ -9,6 +9,7 @@ const Footer: React.FC<FooterProps> = ({ }) => {
     return (
         <div className="footer">
             <a href="https://github.com/maanbanaan/mental-math-app">GitHub</a>
+            <a href="https://www.paypal.com/ncp/payment/389TL2FRZYWAC">Tip jar</a>
         </div>
     );
 };
