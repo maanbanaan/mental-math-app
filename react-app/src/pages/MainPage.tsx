56e06dff0ed790918b200ed127db206dc50cb171
maanbanaan <manuel.dessing@gmail.com>
2024-11-24 18:56:02 +0100

    reverted buggy change

--- react-app/src/pages/MainPage.tsx
+++ react-app/src/pages/MainPage.tsx
@@ -125,7 +125,6 @@ const MainPage: React.FC = () => {
                     handleSubmit={handleSubmit}
                     score={score}
                     resetScore={resetScore}
-                    generateNewQuestion={generateNewQuestion}
                 />
 
                 <div className="toggle-buttons">
