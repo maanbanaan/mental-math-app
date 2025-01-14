093698346806d005d08e10007413fab8a55d36ec
maanbanaan <manuel.dessing@gmail.com>
2025-01-14 11:39:44 +0100

    hide footer button

--- react-app/src/components/Footer.tsx
+++ react-app/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC<FooterProps> = ({ }) => {
     return (
         <div className="footer">
             <a href="https://github.com/maanbanaan/mental-math-app" target="_blank" rel="noopener noreferrer">GitHub 🚀</a>
-            <a href="https://www.paypal.com/ncp/payment/389TL2FRZYWAC" target="_blank" rel="noopener noreferrer">Tip jar ❤️</a>
+            {/* <a href="https://www.paypal.com/ncp/payment/389TL2FRZYWAC" target="_blank" rel="noopener noreferrer">Tip jar ❤️</a> */}
         </div>
     );
 };
